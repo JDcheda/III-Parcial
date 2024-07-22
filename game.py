4feba1a9e9b75ccd3673765c5b1d525ad15e4a96
JDcheda <jdmejia1@uth.hn>
2024-07-22 16:43:07 -0600

    Agregadas las variables e imagenes de jugador, enemigo y bala

--- game.py
+++ game.py
@@ -24,3 +24,39 @@ nivel = 1
 fuente_game_over = pygame.font.Font('freesansbold.ttf', 64)
 fuente_terminado = pygame.font.Font('freesansbold.ttf', 40)
 fuente_menu = pygame.font.Font('freesansbold.ttf', 32)
+fuente_instrucciones = pygame.font.Font('freesansbold.ttf', 24)
+
+mixer.music.load('data/background.wav')
+mixer.music.play(-1)
+
+imagenes_jugador = ['data/spaceship.png', 'datas/spaceship1.png', 'data/spaceship2.png']
+indice_imagen_jugador = 0
+imagen_jugador = pygame.image.load(imagenes_jugador[indice_imagen_jugador])
+jugador_X = 370
+jugador_Y = 523
+jugador_Xcambio = 0
+
+imagen_invasor = []
+invasor_X = []
+invasor_Y = []
+invasor_Xcambio = []
+invasor_Ycambio = []
+num_invasores = 10
+for num in range(num_invasores):
+    imagen_invasor.append(pygame.image.load('data/alien.png'))
+    invasor_X.append(random.randint(60, 700))
+    invasor_Y.append(random.randint(30, 180))
+    invasor_Xcambio.append(0.3)
+    invasor_Ycambio.append(40)
+
+imagen_bala = pygame.image.load('data/bullet.png')
+bala_X = 0
+bala_Y = 500
+bala_Xcambio = 0
+bala_Ycambio = 3
+estado_bala = "reposo"
+
+def esColision(x1, x2, y1, y2):
+    distancia = math.sqrt((math.pow(x1 - x2, 2)) + (math.pow(y1 - y2, 2)))
+    return distancia <= 50
+
