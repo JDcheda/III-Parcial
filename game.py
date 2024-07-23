8dc09795e39938ae6b4fef4dc5d9343965a5efe2
JDcheda <jdmejia1@uth.hn>
2024-07-23 16:47:02 -0600

    Agregadas las funciones: colision, jugador, invasor, bala, mostrar puntuacion, nivel, vidas y pantallas de Game Over y Partida Terminada

--- game.py
+++ game.py
@@ -60,3 +60,39 @@ def esColision(x1, x2, y1, y2):
     distancia = math.sqrt((math.pow(x1 - x2, 2)) + (math.pow(y1 - y2, 2)))
     return distancia <= 50
 
+def jugador(x, y):
+    pantalla.blit(imagen_jugador, (x - 16, y - 10))
+
+def invasor(x, y, i):
+    pantalla.blit(imagen_invasor[i], (x, y))
+
+def bala(x, y):
+    global estado_bala
+    pantalla.blit(imagen_bala, (x, y))
+    estado_bala = "disparo"
+
+def mostrar_puntuacion(x, y):
+    puntuacion = fuente.render("Puntos: " + str(puntuacion_valor), True, (255, 255, 255))")
+    pantalla.blit(puntuacion, (x, y))
+
+def mostrar_vidas(x, y):
+    vidas_texto = fuente.render("Vidas: " + str(vidas), True, (255, 255, 255))
+    pantalla.blit(vidas_texto, (x, y))
+
+def mostrar_nivel(x, y):
+    nivel_texto = fuente.render("Nivel: " + str(nivel), True, (255, 255, 255))
+    pantalla.blit(nivel_texto, (x, y))
+
+def fin_juego():
+    game_over_texto = fuente_game_over.render("GAME OVER", True, (255, 255, 255))
+    pantalla.blit(game_over_texto, (190, 250))
+    pygame.display.update()
+    time.sleep(3)
+    regresar_al_menu()
+
+def partida_terminada():
+    terminado_texto = fuente_terminado.render("Partida terminada", True,(255, 255, 255))
+    pantalla.blit(terminado_texto, (190, 250))
+    pygame.display.update()
+    time.sleep(3)
+    regresar_al_menu()
